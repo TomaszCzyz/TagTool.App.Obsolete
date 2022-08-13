11c7256cd7dbd71a9f6b1cd72be3712a1fdad758
tczyz <tomczyz97@gmail.com>
2022-08-13 17:01:06 +0200

    add grpc to communicate with backend process

--- TagTool.App/MauiProgram.cs
+++ TagTool.App/MauiProgram.cs
@@ -1,18 +1,27 @@
-﻿namespace TagTool.App;
+using TagTool.App.Services;
+using TagTool.Backend;
+
+namespace TagTool.App;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		return builder.Build();
-	}
+        builder.Services.AddScoped(_ => // todo: make sure if AddSingleton would not be better here 
+        {
+            var channel = UnixDomainSocketConnectionFactory.CreateChannel();
+            return new TagToolService.TagToolServiceClient(channel);
+        });
+
+        return builder.Build();
+    }
 }
