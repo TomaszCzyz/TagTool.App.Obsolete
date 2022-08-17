40541efccbc746007a399ee949287491b17518f1
tczyz <tomczyz97@gmail.com>
2022-08-18 00:21:38 +0200

    change search to partial matching

--- TagTool.App/Pages/MainPageViewModel.cs
+++ TagTool.App/Pages/MainPageViewModel.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.ObjectModel;
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using TagTool.App.Helpers;
 using TagTool.Backend;
 
 namespace TagTool.App.Pages;
+
+public record HighlightInfo(int StartIndex, int Length);
 
 public partial class MainPageViewModel : ObservableObject, IDisposable
 {
@@ -18,11 +21,11 @@ public partial class MainPageViewModel : ObservableObject, IDisposable
     {
         _logger = logger;
         _tagSearchServiceClient = tagSearchServiceClient;
-        _tagsSearchResults = new ObservableCollection<string>();
+        _tagsSearchResults = new ObservableCollection<ViewListItem>();
     }
 
     [ObservableProperty]
-    private ObservableCollection<string> _tagsSearchResults;
+    private ObservableCollection<ViewListItem> _tagsSearchResults;
 
     [ObservableProperty]
     private string _btnText = "Click me!";
@@ -34,24 +37,46 @@ public partial class MainPageViewModel : ObservableObject, IDisposable
 
     private int _count;
 
-    async partial void OnSearchBarTextChanged(string value)
+    async partial void OnSearchBarTextChanged(string value) // todo: make sure that async void won't be a problem here
     {
+        // todo: throttle this method to avoid too many calls
         _tagsSearchResults.Clear();
         _cts?.Cancel();
         _cts?.Dispose();
         _cts = new CancellationTokenSource();
 
-        var findTagsRequest = new FindTagsRequest { PartialTagName = value, MaxReturn = 10 };
-        var callOptions = new CallOptions(cancellationToken: _cts.Token);
+        var matchTagsRequest = new MatchTagsRequest { PartialTagName = value, MaxReturn = 50 };
+        var callOptions = new CallOptions().WithCancellationToken(_cts.Token);
 
-        using var streamingCall = _tagSearchServiceClient.FindTags(findTagsRequest, callOptions);
+        using var streamingCall = _tagSearchServiceClient.MatchTags(matchTagsRequest, callOptions);
 
         try
         {
             while (await streamingCall.ResponseStream.MoveNext(_cts.Token))
             {
-                var findTagsReply = streamingCall.ResponseStream.Current;
-                _tagsSearchResults.Add(findTagsReply.TagName);
+                var reply = streamingCall.ResponseStream.Current;
+
+                var viewListItem = new ViewListItem
+                {
+                    Text = reply.MatchedTagName,
+                    Score = reply.LongestMatch * 10 - reply.LongestMatchStartIndex,
+                    HighlightInfos = reply.MatchedParts.Select(match => new HighlightInfo(match.StartIndex, match.Length))
+                };
+                
+                _tagsSearchResults.Add(viewListItem);
+
+                // var index = _tagsSearchResults
+                //     .ToList()
+                //     .BinarySearch(viewListItem, ViewListItem.ScoreComparer); // todo: optimize
+                //
+                // if (index >= 0)
+                // {
+                //     _tagsSearchResults.Insert(index, viewListItem);
+                // }
+                // else
+                // {
+                //     _tagsSearchResults.Insert(_tagsSearchResults.Count + index, viewListItem);
+                // }
             }
         }
         catch (RpcException e) when (e.Status.StatusCode == StatusCode.Cancelled)
